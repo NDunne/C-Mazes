c217201a0b69e7cb6c8c6c310d8c7a43da2715be
NDunne <24ndunne24@gmail.com>
2019-05-20 13:53:26 +0100

    something works

--- MazeGeneration/RecDivMaze/RecDivMaze.cpp
+++ MazeGeneration/RecDivMaze/RecDivMaze.cpp
@@ -27,7 +27,7 @@ void RecDivMaze::generate()
 {
 	drawBase();
 	
-	splitContainer({ 1, 1, X_NODES-1, Y_NODES-1 });
+	splitContainer({ 1, 1, X_NODES-2, Y_NODES-2 }); //Coordinates are inclusive
 
 }
 
@@ -37,7 +37,7 @@ void RecDivMaze::splitContainer(nodeContainer currentNodes)
 	int numY = currentNodes.yLimit - currentNodes.yStart + 1;
 
 	std::cout << "splitContainer";
-	if (currentNodes.xStart == currentNodes.xLimit && currentNodes.yStart == currentNodes.yLimit)
+	if (numX <= 2 || numY <= 2)
 	{
 		return;
 	}
@@ -47,23 +47,40 @@ void RecDivMaze::splitContainer(nodeContainer currentNodes)
 
 		//Vertical or horizontal wall
 
-		int options = numX + numY;
-
-		int choice = rand() % options;
-
-		if (choice < numX)
+		//choice is the index of the wall
+		int choice;
+		if (numY < numX)
 		{
-			drawVWall(currentNodes.xStart + choice, currentNodes.yStart, currentNodes.yLimit);
-			splitContainer({currentNodes.xStart,currentNodes.yStart,currentNodes.xStart + choice - 1,currentNodes.yLimit });
-			splitContainer({currentNodes.xStart + choice + 1, currentNodes.yStart,currentNodes.xLimit,currentNodes.yLimit });
+			choice = currentNodes.xStart + (rand() % (numX - 2)) + 1; //-2 to ensure 2 containers
+			drawVWall(choice, currentNodes.yStart, currentNodes.yLimit);
+			splitContainer({currentNodes.xStart,currentNodes.yStart,choice-1,currentNodes.yLimit });
+			splitContainer({choice+1, currentNodes.yStart,currentNodes.xLimit,currentNodes.yLimit });
+		}
+		else if (numX < numY)
+		{
+			choice = currentNodes.xStart + (rand() % (numY - 2)) + 1; //-2 to ensure 2 containers
+			drawHWall(currentNodes.xStart, currentNodes.xLimit, choice);
+			splitContainer({ currentNodes.xStart, currentNodes.yStart, currentNodes.xLimit, choice-1});
+			splitContainer({currentNodes.xStart, choice+1, currentNodes.xLimit, currentNodes.yLimit});
 		}
 		else
 		{
-			drawHWall(currentNodes.xStart, currentNodes.xLimit, currentNodes.yStart + (choice - numX));
-			splitContainer({ currentNodes.xStart, currentNodes.yStart, currentNodes.xLimit, currentNodes.yStart + (choice - numX) - 1 });
-			splitContainer({currentNodes.xStart, currentNodes.yStart + (choice - numX) - 1, currentNodes.xLimit, currentNodes.yLimit});
+			choice = rand() % ((numX + numY) - 4);
+			if (choice < numX)
+			{
+				choice = currentNodes.xStart + (rand() % (numX - 2)) + 1; //-2 to ensure 2 containers
+				drawVWall(choice, currentNodes.yStart, currentNodes.yLimit);
+				splitContainer({ currentNodes.xStart,currentNodes.yStart,choice - 1,currentNodes.yLimit });
+				splitContainer({ choice + 1, currentNodes.yStart,currentNodes.xLimit,currentNodes.yLimit });
+			}
+			else
+			{
+				choice = currentNodes.xStart + (rand() % (numY - 2)) + 1; //-2 to ensure 2 containers
+				drawHWall(currentNodes.xStart, currentNodes.xLimit, choice);
+				splitContainer({ currentNodes.xStart, currentNodes.yStart, currentNodes.xLimit, choice - 1 });
+				splitContainer({ currentNodes.xStart, choice + 1, currentNodes.xLimit, currentNodes.yLimit });
+			}
 		}
-
 		SDL_Delay(500);
 	}
 }
@@ -74,7 +91,7 @@ void RecDivMaze::drawHWall(int x1, int x2, int y)
 
 	for (int i = x1; i <= x2; i++)
 	{
-		if (i != door)
+		if (i != x1 + door)
 		{
 			setCellType(i,y,WALL);
 		}
@@ -87,7 +104,7 @@ void RecDivMaze::drawVWall(int x, int y1, int y2)
 
 	for (int i = y1; i <= y2; i++)
 	{
-		if (i != door)
+		if (i != y1 + door)
 		{
 			setCellType(x,i, WALL);
 		}
